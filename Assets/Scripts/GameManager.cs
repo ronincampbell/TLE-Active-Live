8405e8371b9e5ccdc21a9f5c556c8ab2d2387366
ronincampbell <ronincampbell3@gmail.com>
2023-08-05 22:50:39 +1000

    Fixed Level Ordering

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -109,6 +109,9 @@ public class GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P) && isPaused)
         {
             Unpause();
+        } else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
         }
     }
 
